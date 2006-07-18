f9c38f4a730bace402bfef80060254dd0d0f1ef5
siafu86 <siafu86@a8ebc1f5-d704-0410-a9ec-c9bfc824b517>
2006-07-18 05:32:05 +0000

    Updated UI nearly working with Dialog Block Upload of first initial ScreenShot1.png

---
+++ trunk/rigo/src/UI/rigo.cpp
@@ -0,0 +1,109 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        rigo.cpp
+// Purpose:     
+// Author:      Jeremy W
+// Modified by: 
+// Created:     07/17/06 22:30:36
+// RCS-ID:      
+// Copyright:   Copyright (C) 2006
+// Licence:     
+/////////////////////////////////////////////////////////////////////////////
+
+#if defined(__GNUG__) && !defined(__APPLE__)
+#pragma implementation "rigo.h"
+#endif
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+////@begin includes
+////@end includes
+
+#include "rigo.h"
+
+////@begin XPM images
+
+////@end XPM images
+
+/*!
+ * Application instance implementation
+ */
+
+////@begin implement app
+IMPLEMENT_APP( RigoApp )
+////@end implement app
+
+/*!
+ * RigoApp type definition
+ */
+
+IMPLEMENT_CLASS( RigoApp, wxApp )
+
+/*!
+ * RigoApp event table definition
+ */
+
+BEGIN_EVENT_TABLE( RigoApp, wxApp )
+
+////@begin RigoApp event table entries
+////@end RigoApp event table entries
+
+END_EVENT_TABLE()
+
+/*!
+ * Constructor for RigoApp
+ */
+
+RigoApp::RigoApp()
+{
+////@begin RigoApp member initialisation
+////@end RigoApp member initialisation
+}
+
+/*!
+ * Initialisation for RigoApp
+ */
+
+bool RigoApp::OnInit()
+{    
+////@begin RigoApp initialisation
+    // Remove the comment markers above and below this block
+    // to make permanent changes to the code.
+
+#if wxUSE_XPM
+    wxImage::AddHandler( new wxXPMHandler );
+#endif
+#if wxUSE_LIBPNG
+    wxImage::AddHandler( new wxPNGHandler );
+#endif
+#if wxUSE_LIBJPEG
+    wxImage::AddHandler( new wxJPEGHandler );
+#endif
+#if wxUSE_GIF
+    wxImage::AddHandler( new wxGIFHandler );
+#endif
+    rigoMainFrame* mainWindow = new rigoMainFrame( NULL, Rigo );
+    mainWindow->Show(true);
+////@end RigoApp initialisation
+
+    return true;
+}
+
+/*!
+ * Cleanup for RigoApp
+ */
+int RigoApp::OnExit()
+{    
+////@begin RigoApp cleanup
+    return wxApp::OnExit();
+////@end RigoApp cleanup
+}
+
---
+++ trunk/rigo/src/UI/rigo.h
@@ -0,0 +1,80 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        rigo.h
+// Purpose:     
+// Author:      Jeremy W
+// Modified by: 
+// Created:     07/17/06 22:30:36
+// RCS-ID:      
+// Copyright:   Copyright (C) 2006
+// Licence:     GPL v2
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _RIGO_H_
+#define _RIGO_H_
+
+#if defined(__GNUG__) && !defined(__APPLE__)
+#pragma interface "rigo.cpp"
+#endif
+
+/*!
+ * Includes
+ */
+
+////@begin includes
+#include "wx/image.h"
+#include "mainFrame.h"
+////@end includes
+
+/*!
+ * Forward declarations
+ */
+
+////@begin forward declarations
+////@end forward declarations
+
+/*!
+ * Control identifiers
+ */
+
+////@begin control identifiers
+////@end control identifiers
+
+/*!
+ * RigoApp class declaration
+ */
+
+class RigoApp: public wxApp
+{    
+    DECLARE_CLASS( RigoApp )
+    DECLARE_EVENT_TABLE()
+
+public:
+    /// Constructor
+    RigoApp();
+
+    /// Initialises the application
+    virtual bool OnInit();
+
+    /// Called on exit
+    virtual int OnExit();
+
+////@begin RigoApp event handler declarations
+////@end RigoApp event handler declarations
+
+////@begin RigoApp member function declarations
+////@end RigoApp member function declarations
+
+////@begin RigoApp member variables
+////@end RigoApp member variables
+};
+
+/*!
+ * Application instance declaration 
+ */
+
+////@begin declare app
+DECLARE_APP(RigoApp)
+////@end declare app
+
+#endif
+    // _RIGO_H_
